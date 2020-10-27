6c0c6dd092610117016c74903a485de077c4cf4f
MartinsMessias <messiasads2017@gmail.com>
2020-10-27 14:24:00 -0500

    Refatorando

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable no-use-before-define */
 import React from 'react';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
