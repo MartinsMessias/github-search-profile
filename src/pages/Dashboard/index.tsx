b7fbadfb3e1c38cff24235bccd3b1ae6e0b54f8b
MartinsMessias <messiasads2017@gmail.com>
2020-11-04 20:15:53 -0500

    Estilizando a dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,62 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import { Title } from './styles';
+
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImage from '../../assets/logo.svg';
+import Repository from '../Repository';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/35672212?s=460&u=fc42b4ce1eb30cd0bd84c5a94dad5a5c53071bf3&v=4"
+            alt="Dat"
+          />
+          <div>
+            <strong>martinsmessias/gostack</strong>
+            <p>Easy game bro!</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="#teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/35672212?s=460&u=fc42b4ce1eb30cd0bd84c5a94dad5a5c53071bf3&v=4"
+            alt="Dat"
+          />
+          <div>
+            <strong>martinsmessias/gostack</strong>
+            <p>Easy game bro!</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="#teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/35672212?s=460&u=fc42b4ce1eb30cd0bd84c5a94dad5a5c53071bf3&v=4"
+            alt="Dat"
+          />
+          <div>
+            <strong>martinsmessias/gostack</strong>
+            <p>Easy game bro!</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
